36e30edd29605300c892a8daa1118159bee4ff49
JustMe080 <hannon.soriano1227@gmail.com>
2025-02-27 03:22:18 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
-from flask import Flask, request, jsonify, make_response
-from flask_cors import CORS  
-from transformers import T5ForConditionalGeneration, T5Tokenizer
+from flask import Flask, request, jsonify
+import requests
+import os
+from dotenv import load_dotenv
 
-app = Flask(__name__)
-CORS(app)  
+load_dotenv()
 
-model_path = 'JexCaber/Translingo' 
-model = T5ForConditionalGeneration.from_pretrained(model_path)
-tokenizer = T5Tokenizer.from_pretrained(model_path)
+app= Flask(__name__)
+
+API_URL = "https://api-inference.huggingface.co/models/JexCaber/TransLingo"
+
+HEADERS ={"Authorization": f"Bearer {os.getenv('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')}"}
 
 @app.route("/simplify-text", methods=['POST'])
 def simplify_text():
-    input_text = request.json.get('text', '')
+    try:
+        data = request.json
+        text =data.get('text')
 
-    inputs = tokenizer(input_text, return_tensors="pt")
-    outputs = model.generate(**inputs, max_length=50, top_k=50)
-    simplified_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        if not text:
+            return({"error": "No Text Provided"}), 400
+        
+        response = requests.post(API_URL, header = HEADERS, json={"inputs":text})
+        response.raise_for_status()
+        return jsonify(response.json())
+    except requests.exception.RequestException as e:
+        return jsonify({"error":str(e)}), 500
+    
 
-    print(simplified_text)
-    return make_response(jsonify({"simplifiedText": simplified_text}), 200)
+def home():
+    return "Welcome"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
