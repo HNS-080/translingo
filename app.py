e6972a62cfc1d2f0540862d8934b3788ca5d0224
HNS-080 <hannon.soriano1202@gmail.com>
2025-03-30 00:20:35 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ df = pd.read_csv(csv_path)
 definitions = dict(zip(df['TERMS'], df['Definition']))
 
 # Hugging Face API details for text simplification
-API_URL1 = "https://huggingface.co/JustMe080/simplification"
-API_URL2 = "https://huggingface.co/JustMe080/Terms"
+API_URL1 = "https://api-inference.huggingface.co/models/JustMe080/simplification"
+API_URL2 = "https://api-inference.huggingface.co/models/JustMe080/Terms"
 HEADERS = {"Authorization": f"Bearer {os.getenv('HUGGINGFACE_TOKEN')}"}
 HEADERS2 = {"Authorization": f"Bearer {os.getenv('HUGGINGFACE_TOKEN2')}"}
 
